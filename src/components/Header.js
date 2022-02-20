8a341c0e126f99eab30d9846753a50040645c75f
Atul Kumar Pandey <pandeyatul0095@gmail.com>
2022-02-20 07:18:38 +0530

    completed

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+import logo from "../images/troll-face.png"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src={logo}
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project"></h4>
+        </header>
+    )
+}
